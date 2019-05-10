121ab7ab847fdcde930deb38692db6af81d928ac
Jens Janson <hello@jensjanson.com>
2019-05-10 12:43:22 +0100

    Link word mark to collection

--- src/components/LogoWordMark.js
+++ src/components/LogoWordMark.js
@@ -2,14 +2,19 @@ import React from "react"
 import VerticalLogo from "../images/logo/logotype-vertical.svg"
 import HorisontalLogo from "../images/logo/logotype-horisontal.svg"
 import { LogoWMWrapperVertical, LogoWMWrapperHorisontal } from "./Styled"
+import { Link } from "./Transitions"
 
 const LogoWordMark = () => (
   <>
     <LogoWMWrapperVertical>
-      <VerticalLogo />
+      <Link to="/collection">
+        <VerticalLogo />
+      </Link>
     </LogoWMWrapperVertical>
     <LogoWMWrapperHorisontal>
-      <HorisontalLogo />
+      <Link to="/collection">
+        <HorisontalLogo />
+      </Link>
     </LogoWMWrapperHorisontal>
   </>
 )
