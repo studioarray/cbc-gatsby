0da4a189296cdde4a69c9aa794e880955870acab
Jens Janson <hello@jensjanson.com>
2019-07-15 18:41:09 +0100

    Reverted to older version of gatsby source s3 image module

--- gatsby-source-s3-image/index.d.ts
+++
@@ -1,3 +0,0 @@
-import ExifData from './types/ExifData'
-import S3ImageAssetNode from './types/S3ImageAssetNode'
-export { ExifData, S3ImageAssetNode }
--- gatsby-source-s3-image/types/EntityNode.d.ts
+++
@@ -1,7 +0,0 @@
-export default interface EntityNode {
-    absolutePath: string;
-    fileNodeId: string;
-    Key: string;
-    mediaType: string;
-    objectHash: string;
-}
